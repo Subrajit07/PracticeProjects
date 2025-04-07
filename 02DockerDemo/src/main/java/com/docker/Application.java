3481da33ef4483f96ac73141d2562d1d3d126e67
Subrajit07 <subrajitlenka1234@gmail.com>
2025-04-07 11:34:59 +0530

    initial commit

---
+++ 02DockerDemo/src/main/java/com/docker/Application.java
@@ -0,0 +1,14 @@
+package com.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		System.out.println("From Docker project 02");
+	}
+
+}
